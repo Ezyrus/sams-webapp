13a3af9a05274e8eec94c141e7b55396f3be6a31
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2022-10-17 13:46:59 +0800

    Update databaseConnection.php

--- databaseConnection.php
+++ databaseConnection.php
@@ -1,12 +1,12 @@
 <?php
-function databaseConnection() {
-$dbHost = 'localhost';
-$dbName = 'samsdb';
-$dbUsername = 'root';
-$dbPassword = ''; 
-$connection = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
-return $connection;
-}
+    function databaseConnection() {
+        $dbHost = 'localhost';
+        $dbName = 'samsdb';
+        $dbUsername = 'root';
+        $dbPassword = ''; 
+        $connection = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
+        return $connection;
+    }
 ?>
 
 
