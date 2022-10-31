ba170df57f0de00593ab0f2cdd3c18640163b1d5
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2022-10-31 15:05:40 +0800

    Update registerStudents.php

--- registerStudents.php
+++ registerStudents.php
@@ -1,35 +1,35 @@
-<?php 
-   session_start(); 
-   require_once "databaseConnection.php";
+<?php
+session_start();
+require_once "databaseConnection.php";
 
-   if (isset($_POST['registerStudent'])) {
-      $studentLrn = $_POST['lrn'];
-      $studentName = $_POST['name'];
-      $studentSection = $_POST['section'];
-      $studentAge = $_POST['age'];
-      $studentAddress = $_POST['address'];
-      $studentEmail = $_POST['email'];
-      $studentNumber = $_POST['number'];
+if (isset($_POST['registerStudent'])) {
+   $studentLrn = $_POST['lrn'];
+   $studentName = $_POST['name'];
+   $studentSection = $_POST['section'];
+   $studentAge = $_POST['age'];
+   $studentAddress = $_POST['address'];
+   $studentEmail = $_POST['email'];
+   $studentNumber = $_POST['number'];
 
-      $selectStudentSql = "SELECT * FROM students WHERE lrn = '$studentLrn'";
+   $selectStudentSql = "SELECT * FROM students WHERE lrn = '$studentLrn'";
 
-      $initiateSelectSql = mysqli_query(databaseConnection(), $selectStudentSql);
+   $initiateSelectSql = mysqli_query(databaseConnection(), $selectStudentSql);
 
-      $studentTableNumRows = mysqli_num_rows($initiateSelectSql);
-      
-         if ($studentTableNumRows) {
-            echo '<script>alert("Student LRN :  ' . $studentLrn . ' already exist")</script>';
-         } else {
-            if(empty($studentLrn)) {
-               echo '<script>alert("Student LRN field is required")</script>';
-            } else {
-               $insertToDatabase = "INSERT INTO `students` (`lrn`, `name`, `section`, `age`, `address`, `email`, `contact_number`) VALUES ('$studentLrn', '$studentName', '$studentSection', '$studentAge', '$studentAddress', '$studentEmail', '$studentNumber')";
+   $studentTableNumRows = mysqli_num_rows($initiateSelectSql);
 
-               $startInsertion = mysqli_query(databaseConnection(), $insertToDatabase);
-               echo '<script>alert("Student Added")</script>';
-            }
-         }
+   if ($studentTableNumRows) {
+      echo '<script>alert("Student LRN :  ' . $studentLrn . ' already exist")</script>';
+   } else {
+      if (empty($studentLrn)) {
+         echo '<script>alert("Student LRN field is required")</script>';
+      } else {
+         $insertToDatabase = "INSERT INTO `students` (`lrn`, `name`, `section`, `age`, `address`, `email`, `contact_number`) VALUES ('$studentLrn', '$studentName', '$studentSection', '$studentAge', '$studentAddress', '$studentEmail', '$studentNumber')";
+
+         $startInsertion = mysqli_query(databaseConnection(), $insertToDatabase);
+         echo '<script>alert("Student Added")</script>';
+      }
    }
+}
 
 ?>
 
@@ -52,8 +52,8 @@
 </head>
 
 <body>
-    
-    <header>
+
+   <header>
       <div class="calLogo">
          <div>
             <img src="assets/calHigh.png" alt="Caloocan Highschool Logo">
@@ -70,53 +70,111 @@
             <img src="assets/depEd.png" alt="DepEd Logo">
          </div>
       </div>
-    </header>
+   </header>
 
-    <div class="registerContainer">
-        
-        <form action="" method="POST">
+   <section class="nav">
+
+      <div class="legend">
+
+         <div>
+            <div class="present">
+               <h2>present:</h2>
+               <h3 class="color present">#P#</h3>
+            </div>
+
+            <div class="absent">
+               <h2>absent:</h3>
+                  <h3 class="color absent">#A#</h3>
+            </div>
+
+            <div class="schoolDays">
+               <h2>No Class:</h2>
+               <h3 class="color schoolDays">NC#</h3>
+            </div>
+         </div>
+
+         <h1><span class="green">︾ L</span>e<span class="red">ge</span>n<span class="yellow">ds</span></h1>
+
+      </div>
+
+      <div class="title">
+         <h6 class="adminLogged">Admin : <span><?php echo $_SESSION['username']; ?></span></h6>
+
+         <div>
+            <h3>October 2022</h3>
+            <h6>4p's Grade 11 Student Records</h6>
+         </div>
+
+      </div>
+
+      <div class="navigate">
+
+         <div>
+            <h2 class="addStudents"><a href="../addStudents.php">
+                  <?php $_SESSION['monthYear'] = "october2022"; ?>Add students</a>
+            </h2>
+            <h2 class="studentProfile">
+               <a href="../studentProfile.php">Student Profile</a>
+            </h2>
+         </div>
+
+         <div>
+            <h2 class="gradeLevel">
+               <a href="../gradeLevel.php">Grade Level</a>
+            </h2>
+            <h2 class="logout">
+               <a href="../logout.php">logout ︾</a>
+            </h2>
+         </div>
+
+      </div>
+
+   </section>
+
+   <div class="registerContainer">
+
+      <form action="" method="POST">
          <h3>Register Students</h3>
 
-               <div class="studentInfo">
-                  
-                  <div class="mainInfo">
-                     <label for="lrn">LRN</label>
-                     <input type="text" name="lrn">
+         <div class="studentInfo">
 
-                     <label for="name">Name</label>
-                     <input type="text" name="name">
+            <div class="mainInfo">
+               <label for="lrn">LRN</label>
+               <input type="text" name="lrn">
 
-                     <label for="section">Section</label>
-                     <input type="text" name="section">
-                  </div>
+               <label for="name">Name</label>
+               <input type="text" name="name">
 
-                  <div class="otherInfo">
-                     <label for="age">Age</label>
-                     <input type="" name="age">
-
-                     <label for="address">Address</label>
-                     <input type="text" name="address">
-
-                     <label for="email">Email</label>
-                     <input type="text" name="email">
-
-                     <label for="number">Number</label>
-                     <input type="text" name="number">
-                  </div>
-
-               </div>
-
-               <div class="navigate">
-                  <h3><a href="gradeLevel.php">Grade Level</a></h3>
-                  <button type="submit" name="registerStudent">Register</button>
-                  <h3><a href="studentProfile.php">Student Profile > </a></h3>
+               <label for="section">Section</label>
+               <input type="text" name="section">
             </div>
-        </form>
-        
-    </div>
 
+            <div class="otherInfo">
+               <label for="age">Age</label>
+               <input type="" name="age">
 
-    <footer>
+               <label for="address">Address</label>
+               <input type="text" name="address">
+
+               <label for="email">Email</label>
+               <input type="text" name="email">
+
+               <label for="number">Number</label>
+               <input type="text" name="number">
+            </div>
+
+         </div>
+
+         <div class="navigate">
+            <h3><a href="gradeLevel.php">Grade Level</a></h3>
+            <button type="submit" name="registerStudent">Register</button>
+            <h3><a href="studentProfile.php">Student Profile > </a></h3>
+         </div>
+      </form>
+
+   </div>
+
+   <footer>
 
       <div class="fdswdLogo">
          <div>
@@ -149,7 +207,7 @@
       </div>
 
    </footer>
-    
+
 </body>
 
 </html>
