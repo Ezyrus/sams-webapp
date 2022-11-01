f17b310f31e0a9e7721f53ee44e0f7ff7ed7ebea
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2022-11-01 09:00:39 +0800

    Update samsdb.sql

--- samsdb.sql
+++ samsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2022 at 01:33 PM
+-- Generation Time: Nov 01, 2022 at 01:59 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -40,7 +40,8 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 (1, 'cyrusAdmin', 'p@ssword12345'),
 (3, 'paoloAdmin', 'p@ssword12345'),
-(4, 'virayAdmin', 'p@ssword12345');
+(4, 'virayAdmin', 'p@ssword12345'),
+(12, 'araAdmin', 'p@ssword12345');
 
 -- --------------------------------------------------------
 
@@ -49,8 +50,8 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `october2022` (
-  `month_id` int(11) NOT NULL,
-  `lrn` int(30) NOT NULL,
+  `month_id` int(20) NOT NULL,
+  `lrn` varchar(50) NOT NULL,
   `student_name` varchar(50) NOT NULL,
   `section` varchar(20) NOT NULL,
   `1` varchar(10) NOT NULL,
@@ -87,8 +88,20 @@ CREATE TABLE `october2022` (
   `school_days` int(10) NOT NULL,
   `present_total` int(10) NOT NULL,
   `absent_total` int(10) NOT NULL,
+  `attendance_rate` int(10) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `october2022`
+--
+
+INSERT INTO `october2022` (`month_id`, `lrn`, `student_name`, `section`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`, `26`, `27`, `28`, `29`, `30`, `31`, `school_days`, `present_total`, `absent_total`, `attendance_rate`, `timestamp`) VALUES
+(8, '136606090102', 'Monica E. Paredes', 'ICT 11 - 1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '2022-10-28 04:46:26'),
+(9, '136612070392', 'John Edrich Viray', 'STEM 11 - 1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '2022-10-28 04:46:54'),
+(10, '136618070392', 'Paolo Rafael S. Tampico', 'ICT 11 - 1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '2022-10-28 04:47:14'),
+(11, '223505070048', 'Arabella Belardo', 'ICT 11 - 1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '2022-10-28 08:52:20'),
+(12, '136618123456', 'Cyrus C. Cantero', 'STEM 11 - 1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '2022-10-28 08:52:23');
 
 -- --------------------------------------------------------
 
@@ -148,13 +161,13 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `october2022`
 --
 ALTER TABLE `october2022`
-  MODIFY `month_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `month_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
