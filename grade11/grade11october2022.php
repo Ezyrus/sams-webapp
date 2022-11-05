b96a53bdc41fe54f5c6ce10fbd52fbfe1099f8b4
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2022-11-05 17:31:42 +0800

    config

--- grade11/grade11october2022.php
+++ grade11/grade11october2022.php
@@ -21,7 +21,7 @@ $selectTableNumRows = mysqli_num_rows($initiateSelectSql);
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <link rel="stylesheet" href="../styles/grade11october2022.css?v=<?php echo time(); ?>">
+   <link rel="stylesheet" href="../styles/mainAttendanceUI.css?v=<?php echo time(); ?>">
    <link rel="stylesheet" href="../styles/header-footer.css?v=<?php echo time(); ?>">
    <script src="../js/loader.js?v=<?php echo time(); ?>"></script>
    <title>Student Attendance Monitoring System</title>
