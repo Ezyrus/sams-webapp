a14a6d364dbeb996763cbeeb4a1764a5c3c1e3fa
GoodestBoi! <83266402+Ezyrus@users.noreply.github.com>
2022-11-10 13:30:22 +0800

    Update studentAttendanceColor.js

--- js/studentAttendanceColor.js
+++ js/studentAttendanceColor.js
@@ -1,16 +1,16 @@
-var element = document.querySelectorAll('select.classDays').forEach(item => {
+    var element = document.querySelectorAll('select.classDays').forEach(item => {
         item.addEventListener('change', event => {
-              var value = event.target.value;
+            var value = event.target.value;
 
-        var selectElement = event.target;
-        var tableDataElement = selectElement.parentElement;
+            var selectElement = event.target;
+            var tableDataElement = selectElement.parentElement;
 
-            if (value == 'present') {
-                tableDataElement.style.background = 'green';  
-            } else if (value == 'absent') {
-                tableDataElement.style.background = 'red';  
-            } else {
-                tableDataElement.style.background = 'yellow';  
-            }
+                if (value == 'present') {
+                    tableDataElement.style.background = 'green';  
+                } else if (value == 'absent') {
+                    tableDataElement.style.background = 'red';  
+                } else {
+                    tableDataElement.style.background = 'yellow';  
+                }
         })
-      })
+    })
